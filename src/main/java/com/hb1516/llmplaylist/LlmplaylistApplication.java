34a604a79712148da6a3f50064f1ae8b9696ec58
HongBeom <hongbeom7877@naver.com>
2025-09-01 16:32:42 +0900

    first commit

---
+++ src/main/java/com/hb1516/llmplaylist/LlmplaylistApplication.java
@@ -0,0 +1,13 @@
+package com.hb1516.llmplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmplaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmplaylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hb1516/llmplaylist/LlmplaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb1516.llmplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmplaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
